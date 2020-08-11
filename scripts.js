012e2c778873556007d129760f67f891d4077054
danielnoo <dannynoo@gmail.com>
2020-08-11 16:43:59 -0400

    fixed buttons, added link

--- scripts.js
+++ scripts.js
@@ -2,25 +2,33 @@
 
 const soundEffect = new Audio('sms-alert-1-daniel_simon.wav');
 
+let timerRunning = false;
 
-function setTimer(minutes){
-  let timer = minutes;
-  document.getElementById("clock").innerHTML = minutes;
-  const newTimer = setInterval(subtractMinute, 3000)
+let startTimer;
+
+
+function setTimer(startTimer){
+  
+  
+  
+  document.getElementById("clock").innerHTML = startTimer;
+  const newTimer = setInterval(subtractMinute, 1000)
   
   function subtractMinute(){
-    timer -= 1;
-    if(timer === 0){
+    startTimer -= 1;
+    if(startTimer === 0){
       document.getElementById("clock").innerHTML = "SNACK TIME";
       clearInterval(newTimer);
       soundEffect.play();
     } else {
-      document.getElementById("clock").innerHTML = timer;
+      document.getElementById("clock").innerHTML = startTimer;
     };
   };
   
   
 };
+
+
 
 
 
