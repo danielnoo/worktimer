0f20353c19a593ce151d16a73a31bdcd9b0d899b
danielnoo <dannynoo@gmail.com>
2020-08-12 13:02:45 -0400

    changed javascript methods to incorporate javascript onclick features and made pause/resume and reset buttons

--- scripts.js
+++ scripts.js
@@ -1,32 +1,158 @@
 
 
 const soundEffect = new Audio('sms-alert-1-daniel_simon.wav');
+const completeEffect = new Audio('finishtone.mp3')
+
+const fiveMinBreak = 300000;
+const fifteenMinBreak = 900000;
+const shortWorkTimer = 1500000;
+const longWorkTimer = 2700000;
+
+
+
+const timer = document.querySelector("#clock");
+const fiveMinBreakButton = document.querySelector('#shortBreak');
+const fifteenMinBreakButton = document.querySelector('#longBreak');
+const shortWorkTimerButton = document.querySelector('#shortWorkTimer');
+const longWorkTimerButton = document.querySelector('#longWorkTimer');
+const pauseButton = document.querySelector('#pauseButton');
+const resetButton = document.querySelector('#resetButton');
 
 let timerRunning = false;
+let clockTimer;
 
-let startTimer;
+// start the timer for 25 minute work period
+
+shortWorkTimerButton.addEventListener('click', () => {
+  clearInterval(clockTimer);
+  let workDuration = 5;
+  timer.innerHTML = workDuration;
+  timerRunning = true;
+  
+  clockTimer = setInterval(() => {
+    workDuration--;
+    timer.innerHTML = workDuration;
+
+    if(workDuration === 0) {
+      clearInterval(clockTimer);
+      timer.innerHTML = "SNACK TIME";
+      completeEffect.play();
+      pauseButton.innerHTML = "Pause";
+    }
+  }, 1000)
+});
 
 
-function setTimer(startTimer){
+// start the timer for 45 minute work period
+
+longWorkTimerButton.addEventListener('click', () => {
+  clearInterval(clockTimer);
+  let workDuration = 45;
+  timer.innerHTML = workDuration;
+  timerRunning = true;
   
+  clockTimer = setInterval(() => {
+    workDuration--;
+    timer.innerHTML = workDuration;
+
+    if(workDuration === 0) {
+      clearInterval(clockTimer);
+      timer.innerHTML = "SNACK TIME";
+      completeEffect.play();
+      pauseButton.innerHTML = "Pause";
+    }
+  }, 1000)
+});
+
+// short break button
+
+fiveMinBreakButton.addEventListener('click', () => {
+  clearInterval(clockTimer);
+  let workDuration = 5;
+  timer.innerHTML = workDuration;
+  timerRunning = true;
   
-  
-  document.getElementById("clock").innerHTML = startTimer;
-  const newTimer = setInterval(subtractMinute, 1000)
-  
-  function subtractMinute(){
-    startTimer -= 1;
-    if(startTimer === 0){
-      document.getElementById("clock").innerHTML = "SNACK TIME";
-      clearInterval(newTimer);
+  clockTimer = setInterval(() => {
+    workDuration--;
+    timer.innerHTML = workDuration;
+
+    if(workDuration === 0) {
+      clearInterval(clockTimer);
+      timer.innerHTML = "SNACK TIME";
       soundEffect.play();
-    } else {
-      document.getElementById("clock").innerHTML = startTimer;
-    };
-  };
+      pauseButton.innerHTML = "Pause";
+    }
+  }, 1000)
+});
+
+
+// long break button
+
+fifteenMinBreakButton.addEventListener('click', () => {
+  clearInterval(clockTimer);
+  let workDuration = 15;
+  timer.innerHTML = workDuration;
+  timerRunning = true;
   
-  
-};
+  clockTimer = setInterval(() => {
+    workDuration--;
+    timer.innerHTML = workDuration;
+
+    if(workDuration === 0) {
+      clearInterval(clockTimer);
+      timer.innerHTML = "SNACK TIME";
+      soundEffect.play();
+      pauseButton.innerHTML = "Pause";
+    }
+  }, 1000)
+});
+
+// pause and resume the clock with alarm intact
+
+pauseButton.addEventListener('click', () => {
+// resume  
+  if(timerRunning === false) {
+    if(timer.innerHTML === "SNACK TIME") {
+      return;
+    }
+    let workDuration = parseInt(timer.innerHTML);
+    timerRunning = true;
+    clockTimer = setInterval(() => {
+      workDuration--;
+      timer.innerHTML = workDuration;
+      if(workDuration === 0) {
+        clearInterval(clockTimer);
+        timer.innerHTML = "SNACK TIME";
+        soundEffect.play();
+        pauseButton.innerHTML = "Pause";
+        timerRunning = false
+      }
+    }, 1000)
+// pause
+  } else {
+    if(timer.innerHTML === "SNACK TIME") {
+      return;
+    }
+    clearInterval(clockTimer);
+    pauseButton.innerHTML = "Resume";
+    timerRunning = false;
+  }
+});
+
+// reset the clock
+
+resetButton.addEventListener('click', () => {
+  clearInterval(clockTimer);
+  timer.innerHTML = "";
+});
+
+
+
+
+
+
+
+
 
 
 
