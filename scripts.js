3589aac69017c8d6389cfab9f8ff911dba58f8a1
danielnoo <dannynoo@gmail.com>
2020-08-19 12:02:53 -0400

    added burger logic as well as a button pressed effect on timers

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 
 
 const soundEffect = new Audio('sms-alert-1-daniel_simon.wav');
-const completeEffect = new Audio('finishtone.mp3')
+const completeEffect = new Audio('finishtone.mp3');
+const burgerEffect = new Audio('burgerlevelup.mp3');
+
 
 const fiveMinBreak = 300000;
 const fifteenMinBreak = 900000;
@@ -9,98 +11,276 @@ const shortWorkTimer = 1500000;
 const longWorkTimer = 2700000;
 
 
-
+const badges = document.querySelector('#badges');
 const timer = document.querySelector("#clock");
+
+
+// buttons
 const fiveMinBreakButton = document.querySelector('#shortBreak');
 const fifteenMinBreakButton = document.querySelector('#longBreak');
+const thirtyMinBreakButton = document.querySelector('#thirtyBreak');
+const sixtyMinBreakButton = document.querySelector('#sixtyBreak');
 const shortWorkTimerButton = document.querySelector('#shortWorkTimer');
 const longWorkTimerButton = document.querySelector('#longWorkTimer');
 const pauseButton = document.querySelector('#pauseButton');
 const resetButton = document.querySelector('#resetButton');
 
+
 let timerRunning = false;
 let clockTimer;
+let cupCakeCounter = 0;
+let coffeeCounter = 0;
+let burgerCounter = 0;
+
+
+
+  
+function removeButtonPress() {
+  let pressed = document.getElementsByClassName('buttonPressed');
+  if(pressed.length > 0){
+  pressed[0].classList.remove('buttonPressed');
+  };
+};
+
+let burgerQuery = function() {
+  // called after a work period is completed
+  // if user has completed 4 of the same consecutive work periods
+  // since 5am local time 
+  // they are rewarded with a burger badge and a long break -  
+  burgerCounter++;
+  let burger = document.querySelector('#burger');
+    
+  let burgerBadge = document.createElement('img');
+  burgerBadge.src = 'doodle-05.svg';
+  burgerBadge.classList.add('badgeDetails');
+
+  
+
+  function addBurger() {
+    burgerEffect.play();
+    burger.classList.add('burgerBadge');
+    
+    function burgerDelay() {
+      burger.classList.remove('burgerBadge');
+      badges.appendChild(burgerBadge);
+    }
+    
+    setTimeout(burgerDelay, 2000);
+    
+        
+  };
+  
+  setTimeout(addBurger, 2000);
+    
+  
+
+};
+
 
 // start the timer for 25 minute work period
 
 shortWorkTimerButton.addEventListener('click', () => {
+  if(timerRunning === true) {
+    removeButtonPress();
+  };
   clearInterval(clockTimer);
-  let workDuration = 5;
-  timer.innerHTML = workDuration;
-  timerRunning = true;
+  let workDuration = 25;
+  timer.innerHTML = workDuration + ' minutes';
   
+  timerRunning = true;
+  shortWorkTimerButton.classList.add('buttonPressed');
   clockTimer = setInterval(() => {
     workDuration--;
-    timer.innerHTML = workDuration;
-
+    if(workDuration === 1){
+      timer.innerHTML = workDuration + ' minute!!!'
+    } else {
+    timer.innerHTML = workDuration + ' minutes';
+    }
+// work duration is completed so sound and animation play, the earned 
+// badge is appended to the user's stats page
     if(workDuration === 0) {
       clearInterval(clockTimer);
       timer.innerHTML = "SNACK TIME";
       completeEffect.play();
       pauseButton.innerHTML = "Pause";
+      let coffee = document.querySelector('#coffee');
+      coffee.classList.add('coffeeBadge');
+      function removeGrow() {
+        coffee.classList.remove('coffeeBadge');
+        badges.appendChild(coffeeCup);
+      };
+      setTimeout(removeGrow, 2000);
+      
+      let coffeeCup = document.createElement('img');
+      coffeeCup.src = 'doodle-46.svg';
+      coffeeCup.classList.add('badgeDetails');
+      
+     
+      coffeeCounter++;
+      if((coffeeCounter % 4) === 0) {
+        burgerQuery();
+      }
+      removeButtonPress();
     }
-  }, 1000)
+  }, 50)
 });
 
 
 // start the timer for 45 minute work period
 
 longWorkTimerButton.addEventListener('click', () => {
+  if(timerRunning === true) {
+    removeButtonPress();
+  };
   clearInterval(clockTimer);
   let workDuration = 45;
-  timer.innerHTML = workDuration;
+  timer.innerHTML = workDuration + ' minutes';
   timerRunning = true;
-  
+  longWorkTimerButton.classList.add('buttonPressed');
   clockTimer = setInterval(() => {
     workDuration--;
-    timer.innerHTML = workDuration;
-
+    if(workDuration === 1){
+      timer.innerHTML = workDuration + ' minute!!!'
+    } else {
+    timer.innerHTML = workDuration + ' minutes';
+    }
+// work duration is completed so sound and animation play, the earned 
+// badge is appended to the user's stats page
     if(workDuration === 0) {
       clearInterval(clockTimer);
       timer.innerHTML = "SNACK TIME";
       completeEffect.play();
       pauseButton.innerHTML = "Pause";
+      let cupCake = document.querySelector('#cupcake');
+      cupCake.classList.add('imgGrow');
+      function removeGrow() {
+        cupCake.classList.remove('imgGrow');
+        badges.appendChild(cupCakeBadge);
+      };
+      setTimeout(removeGrow, 2000);
+      cupCakeCounter++;
+      let cupCakeBadge = document.createElement('img');
+      cupCakeBadge.src = 'doodle-24.svg';
+      cupCakeBadge.classList.add('badgeDetails');
+      if((cupCakeCounter % 4) === 0) {
+        burgerQuery();
+      };
+      removeButtonPress();
     }
-  }, 1000)
+  }, 60000)
 });
 
 // short break button
 
 fiveMinBreakButton.addEventListener('click', () => {
+  if(timerRunning === true) {
+    removeButtonPress();
+  };
   clearInterval(clockTimer);
   let workDuration = 5;
-  timer.innerHTML = workDuration;
+  timer.innerHTML = workDuration + ' minutes';
+  removeButtonPress();
   timerRunning = true;
-  
+  fiveMinBreakButton.classList.add('buttonPressed');
   clockTimer = setInterval(() => {
     workDuration--;
-    timer.innerHTML = workDuration;
+    if(workDuration === 1){
+      timer.innerHTML = workDuration + ' minute!!!'
+    } else {
+    timer.innerHTML = workDuration + ' minutes';
+    }
 
     if(workDuration === 0) {
       clearInterval(clockTimer);
-      timer.innerHTML = "SNACK TIME";
+      timer.innerHTML = "GET TO IT";
       soundEffect.play();
       pauseButton.innerHTML = "Pause";
     }
-  }, 1000)
+  }, 60000)
 });
 
 
 // long break button
 
 fifteenMinBreakButton.addEventListener('click', () => {
+  if(timerRunning === true) {
+    removeButtonPress();
+  };
   clearInterval(clockTimer);
   let workDuration = 15;
-  timer.innerHTML = workDuration;
+  timer.innerHTML = workDuration + ' minutes';
+  removeButtonPress();
   timerRunning = true;
-  
+  fifteenMinBreakButton.classList.add('buttonPressed');
   clockTimer = setInterval(() => {
     workDuration--;
-    timer.innerHTML = workDuration;
+    if(workDuration === 1){
+      timer.innerHTML = workDuration + ' minute!!!'
+    } else {
+    timer.innerHTML = workDuration + ' minutes';
+    }
 
     if(workDuration === 0) {
       clearInterval(clockTimer);
-      timer.innerHTML = "SNACK TIME";
+      timer.innerHTML = "GET TO IT";
+      soundEffect.play();
+      pauseButton.innerHTML = "Pause";
+    }
+  }, 60000)
+});
+
+// thirty minute break
+
+thirtyMinBreakButton.addEventListener('click', () => {
+  if(timerRunning === true) {
+    removeButtonPress();
+  };
+  clearInterval(clockTimer);
+  let workDuration = 30;
+  timer.innerHTML = workDuration + ' minutes';
+  removeButtonPress();
+  timerRunning = true;
+  thirtyMinBreakButton.classList.add('buttonPressed');
+  clockTimer = setInterval(() => {
+    workDuration--;
+    if(workDuration === 1){
+      timer.innerHTML = workDuration + ' minute!!!'
+    } else {
+    timer.innerHTML = workDuration + ' minutes';
+    }
+
+    if(workDuration === 0) {
+      clearInterval(clockTimer);
+      timer.innerHTML = "GET TO IT";
+      soundEffect.play();
+      pauseButton.innerHTML = "Pause";
+    }
+  }, 1000)
+});
+
+// sixty minute break
+
+sixtyMinBreakButton.addEventListener('click', () => {
+  if(timerRunning === true) {
+    removeButtonPress();
+  };
+  clearInterval(clockTimer);
+  let workDuration = 60;
+  timer.innerHTML = workDuration + ' minutes';
+  removeButtonPress();
+  timerRunning = true;
+  sixtyMinBreakButton.classList.add('buttonPressed');
+  
+  clockTimer = setInterval(() => {
+    workDuration--;
+    if(workDuration === 1){
+      timer.innerHTML = workDuration + ' minute!!!'
+    } else {
+    timer.innerHTML = workDuration + ' minutes';
+    }
+    if(workDuration === 0) {
+      clearInterval(clockTimer);
+      timer.innerHTML = "GET TO IT";
       soundEffect.play();
       pauseButton.innerHTML = "Pause";
     }
@@ -125,7 +305,8 @@ pauseButton.addEventListener('click', () => {
         timer.innerHTML = "SNACK TIME";
         soundEffect.play();
         pauseButton.innerHTML = "Pause";
-        timerRunning = false
+        timerRunning = false;
+        removeButtonPress();
       }
     }, 1000)
 // pause
@@ -139,15 +320,48 @@ pauseButton.addEventListener('click', () => {
   }
 });
 
-// reset the clock
+// reset the clock and reset buttons visually
 
 resetButton.addEventListener('click', () => {
   clearInterval(clockTimer);
-  timer.innerHTML = "";
+  timer.innerHTML = "GET TO IT";
+
+  // use recursion to remove the button pressed effect
+  // from any button that has it
+
+  let pressed = document.getElementsByClassName('buttonPressed');
+  
+  function removeButtonPress() {
+    pressed[0].classList.remove('buttonPressed');
+    if(pressed[0]) removeButtonPress();
+  };
+
+  removeButtonPress();
+
+
 });
 
 
 
+// modal
+
+let ini= document.querySelector('#modal-window');
+
+ini.classList.add("hideModal");
+
+function openModal(){
+  let modal= document.querySelector('#modal-window');
+  modal.classList.remove("preload");
+  modal.classList.add("showModal");
+  
+}
+
+function closeM(){
+
+  let m= document.querySelector('#modal-window');
+  m.classList.remove("showModal");
+  
+}
 
 
 
@@ -155,6 +369,19 @@ resetButton.addEventListener('click', () => {
 
 
 
+// todo
+
+// light up the corresponding break timer after a work period is complete
+
+// logic for resetting burger reward
+// applying burger reward
+
+// light up pause/resume button after pushed
+
+
+// award for hitting personal best - peanut butter badge
+
+// adding behavioural analysis to reward/break earning
 
 
 
